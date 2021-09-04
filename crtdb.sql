e2e0fa0b1e3f0558fb44fc208b33c286733d8bcc
NCMartin <70169751+ncmartin108@users.noreply.github.com>
2021-09-03 20:57:02 -0600

    Update crtdb.sql
    Adding item descriptions and steps.

--- crtdb.sql
+++ crtdb.sql
@@ -4,7 +4,7 @@
 #	Team: Mari Langford-Pavao, Nasley Chumacero-Martin, Matt Horrocks			      			#
 #	CS482 Database Management Systems I															                	#
 #																											                            	#
-#	Description:   This file creates an SQL database with the required tables,		  	#
+#	Description:  This file creates an SQL database with the required tables,		  	  #
 #						foreign keys, and value constraints. The schema for this 		          	#
 #						database is in the Project Part 1 documents for a fictional			        #
 #						company named ABC Media.												                     		#
@@ -12,4 +12,12 @@
 #***********************************************************************************#
 
 
-#
+# Step One: The schema has to be defined so that tables can be inserted.
+
+CREATE SCHEMA abc_media_db;
+
+# Step Two:
+
+# Step Three:
+
+
