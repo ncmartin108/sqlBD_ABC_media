97903aff228ca5aa0a7cf05b0c3d27d71adc2aee
NCMartin <70169751+ncmartin108@users.noreply.github.com>
2021-09-03 20:51:01 -0600

    Create crtdb.sql

---
+++ crtdb.sql
@@ -0,0 +1,15 @@
+#***********************************************************************************#
+#																												                            #
+#	Project: Part 1																						                        #
+#	Team: Mari Langford-Pavao, Nasley Chumacero-Martin, Matt Horrocks			      			#
+#	CS482 Database Management Systems I															                	#
+#																											                            	#
+#	Description:   This file creates an SQL database with the required tables,		  	#
+#						foreign keys, and value constraints. The schema for this 		          	#
+#						database is in the Project Part 1 documents for a fictional			        #
+#						company named ABC Media.												                     		#
+#																								                             				#
+#***********************************************************************************#
+
+
+#
