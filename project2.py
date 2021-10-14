4f19f1e9eab1f25d45b012e263f720c24bc8e22e
kash <69873788+KashkaC@users.noreply.github.com>
2021-10-14 11:48:56 -0600

    problem 1
    started problem 1 but cannot get the parameterized query to work.

--- project2.py
+++ project2.py
@@ -1,7 +1,7 @@
 import mysql.connector # import connector
 
 #make connection to db, change to your local info
-pw="your specific password"
+pw="1moreyearofschool"
 mydb = mysql.connector.connect(
 	host ="localhost",
 	user ="root",
@@ -9,9 +9,28 @@ mydb = mysql.connector.connect(
 	database ="abc_media_db"
 	)
 
-mycursor = mydb.cursor();
-mycursor.execute("SHOW tables;")
-print (mycursor)
+def main():
+	#args = sys.argv[1:]
+	#args list 
+	mycursor = mydb.cursor();
 
-for x in mycursor:
-	print(x)
+	sql="select * from Site where address = '34882 Gleichner Circle';"
+	#sql1="select * from Site where address = %s;"
+	#addr=('34882 Gleichner Circle')
+	#addr=input("Enter address to search for in sites: ")
+	
+	mycursor.execute(sql)
+	#mycursor.execute(sql1,addr)
+
+	myresults=mycursor.fetchall()
+
+	for x in myresults:
+		print (x)
+
+	if(mydb.is_connected()):
+		mydb.close()
+		mycursor.close()
+		print("mysql connection is closed.")
+	
+if __name__=="__main__":
+	main()
